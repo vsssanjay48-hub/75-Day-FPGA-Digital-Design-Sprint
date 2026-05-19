362c2cc80b79098004770bdaba786e2e83f10e9a
sanjay <you@example.com>
2026-05-20 03:07:54 +0530

    week 1 ,day 1 i just study basics also made a and gate programme with stimulation

---
+++ OneDrive/Desktop/75/week 1/src/tb_and_gate.v
@@ -0,0 +1,24 @@
+`timescale 1ns/1ps
+
+module tb_and_gate;
+    reg a, b;
+    wire y;
+
+    and_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, tb_and_gate);
+
+        a=0; b=0; #10;
+        a=0; b=1; #10;
+        a=1; b=0; #10;
+        a=1; b=1; #10;
+
+        $finish;
+    end
+endmodule
